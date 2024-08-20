de8477e00c9e7a59a2d88fe7ddbaf3e4acff5d1c
mcdojoh <john.mcdowell@mcdojoh.com>
2024-08-20 09:49:33 -0500

    - Error messages written to stderr - add case-insensitive search - add IGNORE_CASE envvar to switch case sensitivity: `IGNORE_CASE=1 minigrep to poem.txt`

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ This project is to review and implement the techniques and lessons learned from 
 ## Project Description
 1. Program accepts command line arguments in this form: `$ minigrep <QUERY> <FILE>`
 2. Returns the matched lines from the file:  
-```bash
+```
 $ minigrep frog poem.txt
 Searching for 'frog'
 In file 'poem.txt'
 How public, like a frog
 ```
 3. Should throw an error when there aren't enough arguments passed in
-```bash
+```
 $ minigrep frog
 Problem parsing arguments: not enough arguments
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
+use std::env;
 use std::error::Error;
 use std::fs;
+use std::fmt;
 
 pub fn run(cfg: Config) -> Result<(), Box<dyn Error>>{
+    println!("{}", &cfg);
+
     // read the contents of the file
     let contents = fs::read_to_string(cfg.file)?; // condense to a huh-operator; get the error if present
 
-    for line in search(&cfg.query, &contents){
-        println!("{line}"); // save the live
+    // switch function call based on b_ignore_case
+    let results = if cfg.b_ignore_case {
+        search_case_insensitive(&cfg.query, &contents)
+    } else {
+        search_case_sensitive(&cfg.query, &contents)
+    };
+
+    // iterate over 'results'
+    for line in results{
+        println!("{line}"); // print the line(s) that were found
     }
 
     Ok(()) // no error, all good here.
@@ -14,9 +26,11 @@ pub fn run(cfg: Config) -> Result<(), Box<dyn Error>>{
 
 // this 'pub' stuff could become very tedious, but
 // I suppose that it helps with the safety of everything.
+#[derive(Debug)]
 pub struct Config {
     pub query: String,
-    pub file: String
+    pub file: String,
+    pub b_ignore_case: bool     // prepending 'b_' to make it easier to quickly tell this is a bool
 }
 
 impl Config {
@@ -25,7 +39,20 @@ impl Config {
             return Err("not enough arguments");
         }
 
-        Ok(Config{ query: args[1].clone(), file: args[2].clone() })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        // I know you can just use the position, but this is more explicit
+        Ok(Config{ query: args[1].clone(), file: args[2].clone(), b_ignore_case: ignore_case })
+    }
+}
+
+// this is outside the scope for this project, but really wanted an easy way to
+// print the object now for debug purposes. I am pretty sure that there are things
+// that are wrong or at least do not follow the rust patterns. should update once
+// I get through the trait chapter.
+impl fmt::Display for Config {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "query: {}, file: {}, ignore case: {}", &self.query, &self.file, &self.b_ignore_case)
     }
 }
 
@@ -47,6 +74,24 @@ pub fn search<'a>(query: &str, contents: &'a str,) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str,) -> Vec<&'a str> {
+    // store the results in a vector
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+// the original search is case-sensitive so let's just use that
+pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    search(query, contents)
+}
+
 /** tests **/
 #[cfg(test)]
 mod tests {
@@ -63,6 +108,36 @@ Pick three.";
         assert_eq!(
             vec!["safe, fast, productive."],
             search(query, contents)
+        )
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_sensitive(query, contents)
+        )
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_insensitive(query, contents)
         )
     }
 }
